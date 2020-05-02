59a38703656f4623bfaa0fb20c8805c776f72269
Binara Medawatta <binaramedawatta@gmail.com>
2020-05-02 17:13:15 +0530

    Added basic cpp program

---
+++ meta.h
@@ -0,0 +1,42 @@
+// meta.h
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+void loop();
+
+class Meta{
+	int baud;
+	
+	
+	public :
+		begin(int value){
+			baud = value;
+		}
+		
+		print(int value){ cout << value; }
+		print(string value){ cout << value; }
+		print(double value){ cout << value; }
+		println(int value){ cout << value << endl;}
+		println(string value){ cout << value << endl; }
+		println(double value){ cout << value << endl; }
+		
+};
+
+Meta Serial;
+
+int randomNumber(int upper=0, int lower=0){
+	srand(time(NULL));
+	return ((rand() % (upper - lower + 1)) + lower);
+}
+
+analogRead(int pin){ return randomNumber(1023, 0); }
+digitalRead(int pin){ return randomNumber(5, 0); }
+
+void setup();
+void loop();
+
+int main(){
+	setup();
+	loop();
+	return 0;
+}
---
+++ simulator.cpp
@@ -0,0 +1,32 @@
+#include "meta.h"
+
+// define sensors here
+int sensePin = 0; // analog
+int sensorInput;
+double temp;
+
+void setup(){	
+	Serial.begin(9000);
+}
+
+void loop(){
+	
+	sensorInput = analogRead(sensePin);
+	
+	temp = (double)sensorInput / 1024;   //find percentage of input reading
+	temp = temp * 5;                     //multiply by 5V to get voltage
+	temp = temp - 0.5;                   //Subtract the offset 
+	temp = temp * 100;                   //Convert to degrees 
+	
+	Serial.print("Current Temperature: ");
+	Serial.println(temp);
+	Sleep(1000);
+	
+		
+		
+	
+	
+	loop();
+}
+
+
