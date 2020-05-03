7fdd332ae04983bb8ae8805eaec1961776c8749b
Binara Medawatta <binaramedawatta@gmail.com>
2020-05-04 02:16:42 +0530

    added delayMicroSeconds functiomn

--- meta.h
+++ meta.h
@@ -1,18 +1,14 @@
 // meta.h
 #include <bits/stdc++.h>
-#include <windows.h>
+#include "network.h"
+
 using namespace std;
-void loop();
 
 class Meta{
 	int baud;
 	
-	
 	public :
-		begin(int value){
-			baud = value;
-		}
-		
+		begin(int value){ baud = value; }
 		print(int value){ cout << value; }
 		print(string value){ cout << value; }
 		print(double value){ cout << value; }
@@ -24,21 +20,19 @@ class Meta{
 
 Meta Serial;
 
+int HIGH, high = 1;
+int LOW, low = 0;
+
 int randomNumber(int upper=0, int lower=0){
 	srand(time(NULL));
 	return ((rand() % (upper - lower + 1)) + lower);
 }
 
 analogRead(int pin){ return randomNumber(1023, 0); }
-digitalRead(int pin){ return randomNumber(5, 0); }
+digitalRead(int pin){ return randomNumber(1, 0); }
 
 void delay(int value){ Sleep(value); }
+void delayMicroseconds(int value){ Sleep(value*1000); }
 
 void setup();
 void loop();
-
-int main(){
-	setup();
-	while(1) loop();
-	return 0;
-}
